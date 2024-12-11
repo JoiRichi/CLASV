7f886e36f0b57b3696f366cb2fa6fa471d6044f3
Richard Daodu <lordrichado@gmail.com>
2024-12-11 19:33:42 +0100

    updated setup

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ print('Running setup...')
 
 setup(
     name='CLASV',
-    version='0.1.1',
+    version='0.1.12',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -120,10 +120,10 @@ setup(
     description='CLASV is a pipeline designed for rapidly predicting Lassa virus lineages using a Random Forest model.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    author='Daodu Richard, Awotoro Ebenezer',
+    author='Richard Daodu, Ebenezer Awotoro',
     author_email='lordrichado@gmail.com',
     url='https://github.com/JoiRichi/CLASV/commits?author=JoiRichi',
-    python_requires=">=3.6, <=3.11"
+    python_requires=">=3.6, <=3.12"
 ,
     classifiers=[
         "Programming Language :: Python :: 3",
